09ad1c6f66772ad54a7576c2bedc437586e9329b
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-06-06 00:11:48 +0300

    XX-3. Added SaveLoadManager and images

--- Secret_Album/SaveLoadManager.swift
+++ Secret_Album/SaveLoadManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+final class SaveLoadManager {
+    
+    func saveImage(_ image: UIImage) -> String? {
+        
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        let fileName = UUID().uuidString
+        
+        let fileURL = directory.appendingPathComponent(fileName)
+        
+        
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            try? FileManager.default.removeItem(atPath: fileURL.path)
+        }
+        
+        guard let data = image.jpegData(compressionQuality: 1.0) else { return nil }
+        
+        do {
+            try data.write(to: fileURL)
+            return fileName
+        } catch let error {
+            print("Error \(error.localizedDescription)")
+            return nil
+        }
+    
+    }
+    
+}
