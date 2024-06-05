e2539f1a9fda33b67f36d6b7d82424fcf21db583
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-06-05 23:17:03 +0300

    XX-1. Added project

---
+++ Secret_Album/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Secret_Album
+//
+//  Created by Maryia Dziarkach on 5.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
