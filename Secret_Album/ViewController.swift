0e52c690ee1fe0580111acb16f81accec21b81cb
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-06-05 23:26:07 +0300

    XX-1. Created UI on Start Screen

--- Secret_Album/ViewController.swift
+++ Secret_Album/ViewController.swift
@@ -6,14 +6,79 @@
 //
 
 import UIKit
+import SnapKit
+
+private extension CGFloat {
+    static let buttonsLeftOffset: CGFloat = 20
+    static let buttonsRightInset: CGFloat = 20
+    static let buttonsHeight: CGFloat = 40
+    static let buttonsWidth: CGFloat = 40
+}
 
 class ViewController: UIViewController {
 
+    private let nextButton: UIButton = {
+        let nextButton = UIButton()
+        nextButton.setTitle("→", for: .normal)
+        nextButton.setTitleColor(.black, for: .normal)
+        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 35, weight: .heavy)
+        return nextButton
+    }()
+    
+    private let plusButton: UIButton = {
+        let plusButton = UIButton()
+        plusButton.setTitle("+", for: .normal)
+        plusButton.setTitleColor(.black, for: .normal)
+        plusButton.titleLabel?.font = UIFont.systemFont(ofSize: 40, weight: .heavy)
+        return plusButton
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    private func configureUI() {
+        view.backgroundColor = .lightGray
+        
+        view.addSubview(nextButton)
+        nextButton.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-CGFloat.buttonsRightInset)
+            make.bottom.equalToSuperview().offset(-20)
+            make.height.equalTo(CGFloat.buttonsHeight)
+            make.width.equalTo(CGFloat.buttonsWidth)
+        }
+        
+        let nextButtonAction = UIAction { _ in
+            self.nextButtonPressed()
+        }
+        nextButton.addAction(nextButtonAction, for: .touchUpInside)
+        
+        view.addSubview(plusButton)
+        plusButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(CGFloat.buttonsLeftOffset)
+            make.bottom.equalToSuperview().offset(-25)
+            make.height.equalTo(CGFloat.buttonsHeight)
+            make.width.equalTo(CGFloat.buttonsWidth)
+        }
+        
+        let plusButtonAction = UIAction { _ in
+            self.plusButtonPressed()
+        }
+        plusButton.addAction(plusButtonAction, for: .touchUpInside)
+    }
+    
+    
+    private func nextButtonPressed() {
+        let controller = ScrollingImagesViewController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    private func plusButtonPressed() {
+        let controller = AddingImagesViewController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
 
 }
 
