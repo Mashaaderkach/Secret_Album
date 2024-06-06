7df73f171912b43f5c14a8ebcdc018acb598f832
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-06-06 11:38:45 +0300

    XX-4. Created root controller and navigation controller

--- Secret_Album/SceneDelegate.swift
+++ Secret_Album/SceneDelegate.swift
@@ -13,12 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
+            // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+            // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+            // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(windowScene: windowScene) // создание объекта
+            // конфигурирование объекта
+            let controller = ViewController() // создаем корневой контроллер
+            let navigationController = UINavigationController(rootViewController: controller) // создаем navigationController с этим корневым контроллером
+            navigationController.isNavigationBarHidden = true // прячем navigationBar
+            window?.rootViewController = navigationController // у окна приложения есть корневой контроллер (он мб любым). корневой контроллер = navigationController
+            //отображение окна для юзера
+            window?.makeKeyAndVisible() // отображаем это окно (делаем видимым для пользователя)
+        }
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
